ee89dc1f88731dbc19fa4fdc2ecc581b47db256a
FrancaTM <francatm@outlook.com>
2019-09-02 19:49:05 -0300

    add location bloc and location screen

---
+++ lib/bloc/location_bloc.dart
@@ -0,0 +1,24 @@
+import 'dart:async';
+
+import 'package:restaurant_finder/DataLayer/location.dart';
+import 'package:restaurant_finder/bloc/bloc.dart';
+
+class LocationBloc implements Bloc {
+  Location _location;
+
+  Location get selectedLocation => _location;
+
+  final _locationController = StreamController<Location>();
+
+  Stream<Location> get locationStream => _locationController.stream;
+
+  void selectLocation(Location location) {
+    _location = location;
+    _locationController.sink.add(location);
+  }
+
+  @override
+  void dispose() {
+    _locationController.close();
+  }
+}
